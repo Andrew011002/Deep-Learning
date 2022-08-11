b1434d14f825e5c4804af917de56719a24b73409
Andrew011002 <andrewholmes011002@gmail.com>
2022-08-11 11:56:11 -0400

    made code more sparse

--- Transformer/attention.py
+++ Transformer/attention.py
@@ -18,72 +18,46 @@ class ScaledDotProductAttention(nn.Module):
         values = torch.matmul(attention, v)
         return values, attention
 
-class AttentionHead(nn.Module):
-
-    def __init__(self, dm, nhead=1, bias=False, dropout=0.1) -> None:
-        super().__init__()
-        
-        if dm % nhead != 0:
-            raise ValueError("The embeddings dimenions (dm) must be divisble by number of heads (nhead).")
-        
-        self.dm = dm
-        self.dk = dm // nhead
-        self.nhead = nhead
-        self.wq = nn.Linear(dm, dm, bias=bias)
-        self.wk = nn.Linear(dm, dm, bias=bias)
-        self.wv = nn.Linear(dm, dm, bias=bias)
-        self.scaled_dot_prod_attn = ScaledDotProductAttention(self.dk, dropout=dropout)
-
-    def forward(self, q, k, v, mask=None):
-        # inshpe : q = (batch_size, q_len, dm) k & v = (batch_size, seq_len, dm)
-        batch_size = q.size(0)
-
-        # project q, k, & v reshape: q = (batch_size, nhead, q_len, dk) k & v = (batch_size, nhead, seq_len, dk)
-        q = self.wq(q).view(batch_size, -1, self.nhead, self.dk).permute(0, 2, 1, 3)
-        k = self.wk(k).view(batch_size, -1, self.nhead, self.dk).permute(0, 2, 1, 3)
-        v = self.wv(v).view(batch_size, -1, self.nhead, self.dk).permute(0, 2, 1, 3)
-
-        # scaled dot prod attn shape: attention = (batch_size, nhead, q_len, seq_len) values = (batch_size, nhead, q_len, dk)
-        values, attention = self.scaled_dot_prod_attn(q, k, v, mask=mask)
-
-        # reshape: values = (batch_size, q_len, dm)
-        values = values.contiguous().view(batch_size, -1, self.dm)
-        return values, attention
-
 class MultiHeadAttention(nn.Module):
 
     def __init__(self, dm, nhead, bias=False, attn_dropout=0.1, dropout=0.1) -> None:
         super().__init__()
 
-        if nhead <= 1:
-            raise ValueError("Number of heads (nheads) must greater than 1. Use AttentionHead for single head use only.")
-
+        if dm % nhead != 0:
+            raise ValueError("Embedding dimensions (dm) must be divisble by number of heads (nheads.")
+        
+        self.dm = dm
+        self.dk = dm // nhead
+        self.nhead = nhead
+        self.wq = nn.Linear(dm, self.dk * nhead, bias=bias)
+        self.wk = nn.Linear(dm, self.dk * nhead, bias=bias)
+        self.wv = nn.Linear(dm, self.dk * nhead, bias=bias)
         self.wo = nn.Linear(dm, dm)
-        self.heads = AttentionHead(dm, nhead=nhead, bias=bias, dropout=attn_dropout)
         self.dropout = nn.Dropout(dropout)
+        self.scaled_dot_prod_attn = ScaledDotProductAttention(self.dk, dropout=attn_dropout)
 
     def forward(self, q, k, v, mask=None):
-        # inshpe : q = (batch_size, q_len, dm) k & v = (batch_size, seq_len, dm)
+        # inshape: q = (batch_size, q_len, dm) k & v = (batch_size, seq_len, dm)
+        batch_size = q.size(0)
 
-        # calculate values & attention
-        values, attention = self.heads(q, k, v, mask=mask)
+        # linear projections into heads shape: q = (batch_size, nheads, q_len, dk) k & v = (batch_size, nheads, seq_len, dk)
+        q = self.wq(q).view(batch_size, -1, self.nhead, self.dk).transpose(1, 2)
+        k = self.wk(k).view(batch_size, -1, self.nhead, self.dk).transpose(1, 2)
+        v = self.wv(v).view(batch_size, -1, self.nhead, self.dk).transpose(1, 2)
 
-        # projection values shape: values = (batch_size, q_len, dm) attention = (batch_size, nhead, q_len, seq_len)
+        # att scores & weights shape: attention = (batch_size, nhead, q_len, seq_len) values = (batch_size, nhead, q_len, dk)
+        values, attention = self.scaled_dot_prod_attn(q, k, v, mask=mask)
+
+        # concat shape: attention (batch_size, nheads, q_len, seq_len) values = (batch_size, q_len, dm)
+        values = values.transpose(1, 2).contiguous().view(batch_size, -1, self.dm)
+
+        # project and dropout shape: values = (batch_size, q_len, dm)
         values = self.wo(values)
         values = self.dropout(values)
         return values, attention
 
+
 if __name__ == "__main__":
-    q = torch.rand((32, 1, 512))
-    k = torch.rand((32, 10, 512))
-    v = k
-
-    # SINGLE HEAD CALCULATION
-    head = AttentionHead(512)
-    values, attention = head(q, k, v)
-    print(attention.size())
-    print(values.size())
-
     q = torch.rand((64, 5, 512))
     k = torch.rand((64, 10, 512))
     v = k
@@ -91,5 +65,6 @@ if __name__ == "__main__":
     # MULTI-HEAD CALCULATION
     multihead = MultiHeadAttention(512, 8)
     values, attention = multihead(q, k, v)
-    print(attention.size())
-    print(values.size())
+    print(attention.size(), values.size())
+    
+
