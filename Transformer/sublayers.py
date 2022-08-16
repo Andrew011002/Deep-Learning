26f4c1b1b8f6862442e58f2f5f04ae9abb79c92a
Andrew011002 <andrewholmes011002@gmail.com>
2022-08-16 18:26:05 -0400

    working on masking

--- Transformer/sublayers.py
+++ Transformer/sublayers.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 
 class ScaledDotProductAttention(nn.Module):
@@ -65,26 +66,58 @@ class MultiHeadAttention(nn.Module):
         return context, attention
 
 
-def create_subsequent_mask(seq_len):
+def create_subsequent_mask(self, seq_len):
     # create a diagnal of 1's (left) 0's (right)
     mask = torch.triu(torch.ones(seq_len, seq_len) == 1)
-    return mask.float().transpose(0, 1)
+    return mask.int().transpose(0, 1)
 
-def create_padded_mask(src, pad_val):
-    pass
+def create_padded_mask(self, seq, pad_val):
+    return seq != pad_val
 
+
+
+def create_subsequent_mask(seq):
+    seq_len = seq.size(1)
+    # create a diagnal of 1's (left) 0's (right)
+    mask = torch.triu(torch.ones(seq_len, seq_len) == 1)
+    return mask.int().transpose(0, 1)
+
+def create_padded_mask(seq, pad_val):
+    return seq != pad_val
+
+def create_mask(src, tgt, pad_val):
+    src_mask = create_padded_mask(src, pad_val)
+    tgt_mask = create_padded_mask(tgt, pad_val) & create_subsequent_mask(tgt)
+    return src_mask, tgt_mask
+    
 
 
 if __name__ == "__main__":
-    q = torch.rand((64, 10, 512))
-    k = torch.rand((64, 10, 512))
-    v = k
+    # q = torch.rand((64, 10, 512))
+    # k = torch.rand((64, 10, 512))
+    # v = k
     
-    mask = create_subsequent_mask(10)
+    # mask = create_subsequent_mask(10)
 
-    # MULTI-HEAD CALCULATION
-    multihead = MultiHeadAttention(512, 8)
-    values, attention = multihead(q, k, v, mask=mask)
+    # # MULTI-HEAD CALCULATION
+    # multihead = MultiHeadAttention(512, 8)
+    # values, attention = multihead(q, k, v, mask=mask)
+
+    inputs = torch.Tensor([[4, 5, 8, 10, 21, 18, 15, 0, 0, 0]])
+    src = inputs
+    tgt = inputs[:, 1:]
+
+    src_mask, tgt_mask = create_mask(src, tgt, pad_val=0)
+    print(src_mask, f"\n{tgt_mask}")
+
+    
+    
+    
+
+
+
+
+    
 
     
 
