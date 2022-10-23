7168104d1a33f298a70285227f877bce12994ccd
andmholm <andmholm@iu.edu>
2022-10-22 22:56:03 -0400

    started byte pair encoder

--- Transformer/preprocess.py
+++ Transformer/preprocess.py
@@ -3,6 +3,7 @@ import re
 import unidecode
 import nltk
 import spacy
+from collections import defaultdict
 from word2number import w2n
 from nltk.corpus import stopwords as STOPWORDS, wordnet
 from nltk.stem import WordNetLemmatizer
@@ -64,7 +65,101 @@ class Tokenizer:
             decoded.append(self.decode_(ids))
         return np.array(decoded, dtype=object)        
 
-class Process:
+class BytePairEncoder:
+
+    def __init__(self, corpus, vocab_size, model=None, special_tokens=None):
+        self.vocab_size = vocab_size
+        self.corpus = corpus
+        self.model = model
+        self.frequencies = self.get_word_freqs(corpus, model)
+        self.characters = self.get_characters(self.frequencies, special_tokens)
+        self.splits = self.generate_splits(self.frequencies)
+
+    def get_word_freqs(self, corpus, model):
+        
+        # get tokenizer for pre-tokenization
+        if model is None:
+            model = "bert-base-uncased"
+        tokenizer = AutoTokenizer.from_pretrained(model)
+
+        frequencies = defaultdict(int)
+        for text in corpus:
+            # get words and their offsets
+            words = tokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(text)
+            # update word frequency
+            for word, offset in words:
+                frequencies[word] += 1
+
+        return frequencies
+    
+    def get_characters(self, frequencies, special_tokens):
+        characters = set()
+        # add any special tokens
+        if special_tokens:
+            for token in special_tokens:
+                characters.add(token)
+
+        # get unique set of all characters in word corpus
+        for word in frequencies.keys():
+            for char in word:
+                # only adds if the character is not in the set
+                characters.add(char)
+
+        return sorted(list(characters))
+
+    def generate_splits(self, frequencies):
+        splits = dict()
+        # create word character break-up pairings
+        for word in frequencies.keys():
+            splits[word] = [char for char in word]
+        return splits
+
+    def compute_max_pair(self, splits, frequencies):
+        pair_frequencies = defaultdict(int)
+        # get the split for all words in the corpus
+        for word, freq in frequencies.items():
+            split = splits[word]
+            n = len(split)
+            # only find pair frequecies for splits larger than 1
+            if n > 1:
+                for i in range(n - 1):
+                    # update pair frequency
+                    pair = (split[i],  split[i + 1])
+                    pair_frequencies[pair] += freq
+
+        # get most common pair
+        max_pair = max(pair_frequencies, key=pair_frequencies.get)
+        return max_pair
+
+    def merge_pair(self, pair, splits, frequencies):
+        
+        # get the split for words
+        for word in frequencies.keys():
+            split = splits[word]
+            n = len(split)
+            # only merge for words with splits larger than 1
+            if n > 1:
+                i = 0
+                # find the pair in the word if it exists
+                while i < n - 1:
+                    # exists in words
+                    if split[i] == pair[0] and split[i + 1] == pair[1]:
+                        # merge
+                        split = split[:i] + [pair[0] + pair[1]] + split[i + 2:]
+                    # not found look further
+                    else:
+                        i += 1
+                # update the split
+                splits[word] = split
+
+        return splits 
+
+    def update(self):
+        pair = self.compute_max_pair(self.splits, self.frequencies)
+        self.splits = self.merge_pair(pair, self.splits, self.frequencies)
+        self.characters.append(pair[0] + pair[1])
+    
+class PreProcess:
 
     def __init__(self) -> None:
         pass
@@ -162,6 +257,17 @@ class Process:
             text = lemmatizer(text)
             data[i] = " ".join(text)
 
+    def normalize(self, data, model=None):
+
+        if model is None:
+            model = "bert-base-uncased"
+
+        tokenizer = AutoTokenizer.from_pretrained(model)
+
+        for i, text in enumerate(data):
+            text = tokenizer.backend_tokenizer.normalizer.normalize_str(text)
+            data[i] = text
+
     def preprocess(self, data, operations=None):
 
         if operations is None:
@@ -178,5 +284,16 @@ class Process:
 
 
 if __name__ == '__main__':
-    pass
+    corpus = ["hello my name is tonimo", "what's for dinner Joe?", 
+            "I think Charles is not better than max", "I am the very best at F1"]
+
+    bce = BytePairEncoder(corpus, 100)
+    print(bce.frequencies, "\n")
+    print(bce.characters, "\n")
+    print(bce.splits, "\n")
+    bce.update()
+    print(bce.characters, "\n")
+    print(bce.splits, "\n")
+    
+    
     
