26b989f1a76b1a8b324cccc430f3ebdfd1633457
andmholm <andmholm@iu.edu>
2022-11-07 23:09:03 -0500

    can save tk, tk pads & truncs, tk id & tokens only

--- Transformer/tokenizer.py
+++ Transformer/tokenizer.py
@@ -4,29 +4,36 @@ processors, trainers, Tokenizer
 
 class WordPieceTokenizer:
 
-    def __init__(self, vocab_size, normalizer, pre_tokenizer, prefix, special_tokens) -> None:
+    def __init__(self, vocab_size, normalizer, pre_tokenizer, prefix, special_tokens, maxlen=None) -> None:
         self.vocab_size = vocab_size
         self.prefix = prefix
         self.special_tokens = special_tokens
         self.tokenizer = self.init_tokenizer(normalizer, pre_tokenizer, prefix)
+        self.maxlen = maxlen
 
     def init_tokenizer(self, normalizer, pre_tokenizer, prefix):
         # build basic structure of WPT
-        tokenizer = Tokenizer(models.WordPiece(unk_token=self.special_tokens["unknown"]))
+        tokenizer = Tokenizer(models.WordPiece(unk_token=self.special_tokens["unk"]))
         tokenizer.normalizer = normalizer
         tokenizer.pre_tokenizer = pre_tokenizer
         tokenizer.decoder = decoders.WordPiece(prefix)
         return tokenizer
 
     def init_post_processor(self):
-        # get sos/eos tokens and ids
-        sos, eos = self.special_tokens["sos"], self.special_tokens["eos"]
-        ids = self.get_vocab() 
+        # get cls/sep tokens and ids
+        cls, sep = self.special_tokens["cls"], self.special_tokens["sep"]
+        ids = self.vocab() 
         # structure how tokens are process for single sentence and sentence pair
-        post_processor = processors.TemplateProcessing(single=f"{sos} $A:0 {eos}:0",
-                                                        pair=f"{sos} $A:0 {eos}:0 $B:1 {eos}:1", 
-                                                        special_tokens=[(sos, ids[sos]), (eos, ids[eos])])
+        post_processor = processors.TemplateProcessing(single=f"{cls} $A:0 {sep}:0",
+                                                        pair=f"{cls} $A:0 {sep}:0 $B:1 {sep}:1", 
+                                                        special_tokens=[(cls, ids[cls]), (sep, ids[sep])])
         self.tokenizer.post_processor = post_processor
+        
+        # enable padding and truncation for encodings
+        if self.maxlen is not None:
+            pad_id = self.vocab().get(self.special_tokens["pad"])
+            self.tokenizer.enable_padding(pad_id=pad_id, length=self.maxlen)
+            self.tokenizer.enable_truncation(max_length=self.maxlen)
 
     def train(self, corpus):
         # train tokenizer to desired vocab size w/ special tokens and desired prefix
@@ -36,7 +43,12 @@ class WordPieceTokenizer:
         # init the post processor for formatting structure of encoded sequences
         self.init_post_processor()
 
-    def encode(self, data, tokens_only=False):
+    def inference(self):
+        # disable padding and truncation
+        self.tokenizer.no_padding()
+        self.tokenizer.no_truncation()
+
+    def encode(self, data, model=False):
         encoded = []
         # encode all sequences to map of metadata
         for sequence in data:
@@ -46,24 +58,30 @@ class WordPieceTokenizer:
             # encode single sentence
             else:
                 encoding = self.tokenizer.encode(sequence)
-            # only add tokens (ignore other metadata)
-            if tokens_only:
-                encoded.append(encoding.tokens)
-            # keep all metadata
+            # add ids for model training
+            if model:
+                encoded.append(encoding.ids)
+            # not for model add tokens
             else:
-                encoded.append(encoding)
+                encoded.append(encoding.tokens)
         return encoded 
 
     def decode(self, data):
         decoded = []
         # decode all encoded token ids
         for encoding in data:
-            sequence = self.tokenizer.decode(encoding.ids)
+            sequence = self.tokenizer.decode(encoding)
             decoded.append(sequence)
         return sequence
 
-    def get_vocab(self):
+    def vocab(self):
         return self.tokenizer.get_vocab()
+
+    def save(self, filename):
+        self.tokenizer.save(f"{filename}.json")
+
+def load_tokenizer(filename):
+    return Tokenizer.from_file(f"{filename}.json")
 
 
 if __name__ == "__main__":
@@ -75,10 +93,15 @@ if __name__ == "__main__":
     norms = normalizers.Sequence([normalizers.Strip(), normalizers.Lowercase(), 
                                 normalizers.StripAccents(), normalizers.NFD()])
     pretoks = pre_tokenizers.Whitespace()
-    special_tokens = dict(unknown="[UNK]", sos="[SOS]", eos="[EOS]", 
+    special_tokens = dict(unk="[UNK]", cls="[CLS]", sep="[SEP]", 
                         pad="[PAD]", mask="[MASK]")
-    tokenizer = WordPieceTokenizer(vocab_size, norms, pretoks, "##", special_tokens=special_tokens)
-    tokenizer.train(corpus)
+    tokenizer = WordPieceTokenizer(vocab_size, norms, pretoks, "##", special_tokens=special_tokens, maxlen=100)
+
+    
+    
+
+
+    
 
     
 
