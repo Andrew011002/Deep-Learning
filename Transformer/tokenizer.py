689c3ebd914bf95cfe82d006441117335b7dde6d
andmholm <andmholm@iu.edu>
2022-11-12 00:34:58 -0500

    cleaned tokenizer made dataset

--- Transformer/tokenizer.py
+++ Transformer/tokenizer.py
@@ -1,47 +1,67 @@
-from transformers import AutoTokenizer
+from typing import Iterable
 from tokenizers import decoders, models, normalizers, pre_tokenizers,\
 processors, trainers, Tokenizer
 
+defaults = {"normalizer": normalizers.Sequence([normalizers.Strip(), normalizers.Lowercase(), 
+                            normalizers.StripAccents(), normalizers.NFD()]),
+            "pretokenizer": pre_tokenizers.Whitespace(),
+            "special tokens": {"unk": "[UNK]", "cls": "[CLS]", "sep": "[SEP]", 
+                                "pad": "[PAD]", "mask": "[MASK]"}
+            }
+
 class WordPieceTokenizer:
 
-    def __init__(self, vocab_size, normalizer, pre_tokenizer, prefix, special_tokens, maxlen=None) -> None:
-        self.vocab_size = vocab_size
-        self.prefix = prefix
-        self.special_tokens = special_tokens
-        self.tokenizer = self.init_tokenizer(normalizer, pre_tokenizer, prefix)
-        self.maxlen = maxlen
+    def __init__(self, vocab=None, unknown_token="[UNK]", prefix="##", normalizer=None, 
+                    pre_tokenizer=None, special_tokens=None): 
+        self.tokenizer = self.init_tokenizer(vocab, unknown_token, prefix, normalizer, 
+                        pre_tokenizer, special_tokens)
+        self.trained = False
 
-    def init_tokenizer(self, normalizer, pre_tokenizer, prefix):
-        # build basic structure of WPT
-        tokenizer = Tokenizer(models.WordPiece(unk_token=self.special_tokens["unk"]))
+    def init_tokenizer(self, vocab, unknown_token, prefix, normalizer, 
+                        pre_tokenizer, special_tokens):
+        # defaults
+        if normalizer is None:
+            normalizer = defaults["normalizer"]
+        if pre_tokenizer is None:
+            pre_tokenizer = defaults["pretokenizer"]
+        if special_tokens is None:
+            self.special_tokens = defaults["special tokens"]
+
+        # set kwargs for retraining
+        self.kwargs = {"unknown_token": unknown_token, "prefix": prefix, "normalizer": normalizer, 
+                        "pre_tokenizer": pre_tokenizer, "special_tokens": special_tokens}
+        
+        # set tokenizer attrs
+        tokenizer = Tokenizer(models.WordPiece(unk_token=unknown_token, vocab=vocab))
         tokenizer.normalizer = normalizer
         tokenizer.pre_tokenizer = pre_tokenizer
         tokenizer.decoder = decoders.WordPiece(prefix)
         return tokenizer
 
-    def init_post_processor(self):
-        # get cls/sep tokens and ids
+    def train(self, size, corpus):
+        # train tokenizer over corpus
+        trainer = trainers.WordPieceTrainer(vocab_size=size, show_progress=False, 
+                                special_tokens=list(self.special_tokens.values()))
+        self.tokenizer.train_from_iterator(corpus, trainer=trainer)
+        self.corpus = corpus
+        self.size = size
+        self.trained = True
+        # init post processor for input sequences
         cls, sep = self.special_tokens["cls"], self.special_tokens["sep"]
-        ids = self.vocab() 
-        # structure how tokens are process for single sentence and sentence pair
-        post_processor = processors.TemplateProcessing(single=f"{cls} $A:0 {sep}:0",
-                                                        pair=f"{cls} $A:0 {sep}:0 $B:1 {sep}:1", 
-                                                        special_tokens=[(cls, ids[cls]), (sep, ids[sep])])
-        self.tokenizer.post_processor = post_processor
-        
-        # enable padding and truncation for encodings
-        if self.maxlen is not None:
-            pad_id = self.vocab().get(self.special_tokens["pad"])
-            self.tokenizer.enable_padding(pad_id=pad_id, length=self.maxlen)
-            self.tokenizer.enable_truncation(max_length=self.maxlen)
+        cls_id, sep_id = self.tokenizer.token_to_id(cls), self.tokenizer.token_to_id(sep)
+        self.tokenizer.post_processor = processors.TemplateProcessing(
+                                single=f"{cls}:0 $A:0 {sep}:0",
+                                pair=f"{cls}:0 $A:0 {sep}:0 $B:1 {sep}:1",
+                                special_tokens=[(cls, cls_id), (sep, sep_id)])
 
-    def train(self, corpus):
-        # train tokenizer to desired vocab size w/ special tokens and desired prefix
-        trainer = trainers.WordPieceTrainer(vocab_size=self.vocab_size, special_tokens=list(self.special_tokens.values()), 
-                                            continuing_subword_prefix=self.prefix)
-        Tokenizer.train_from_iterator(self.tokenizer, corpus, trainer)
-        # init the post processor for formatting structure of encoded sequences
-        self.init_post_processor()
+    def pruncate(self, maxlen, end=True):
+        pad = self.special_tokens["pad"]
+        pad_id = self.tokenizer.token_to_id(pad)
+        direction = "right" if end else "left"
+        # enable padding and truncation for maxlen
+        self.tokenizer.enable_padding(direction=direction, pad_id=pad_id, 
+                                    pad_token=pad, length=maxlen)
+        self.tokenizer.enable_truncation(max_length=maxlen, direction=direction)
 
     def inference(self):
         # disable padding and truncation
@@ -49,30 +69,39 @@ class WordPieceTokenizer:
         self.tokenizer.no_truncation()
 
     def encode(self, data, model=False):
+        # single sequence input
+        if isinstance(data, str):
+            tokens = self.tokenizer.encode(data)
+            return tokens.ids if model else tokens.tokens
+        # pair of sequence inputs
+        if isinstance(data, tuple) and len(data) == 2:
+            tokens = self.tokenizer.encode(*data)
+            return tokens.ids if model else tokens.tokens
         encoded = []
-        # encode all sequences to map of metadata
+        # list of sequences/sequence pairs
         for sequence in data:
-            # encode sequence pair
-            if isinstance(sequence, tuple):
-                encoding = self.tokenizer.encode(*sequence)
-            # encode single sentence
+            if isinstance(data, tuple):
+                tokens = self.tokenizer.encode(*sequence)
             else:
-                encoding = self.tokenizer.encode(sequence)
-            # add ids for model training
+                tokens = self.tokenizer.encode(sequence)
+            # inputs for model or for reading
             if model:
-                encoded.append(encoding.ids)
-            # not for model add tokens
+                encoded.append(tokens.ids)
             else:
-                encoded.append(encoding.tokens)
-        return encoded 
+                encoded.append(tokens.tokens)
+        return encoded
 
     def decode(self, data):
+        # single encoded ids
+        if isinstance(data[0], int):
+            text = self.tokenizer.decode(data)
+            return text
         decoded = []
-        # decode all encoded token ids
-        for encoding in data:
-            sequence = self.tokenizer.decode(encoding)
-            decoded.append(sequence)
-        return sequence
+        # list of encoded ids
+        for encodings in data:
+            text = self.tokenizer.decode(encodings)
+            decoded.append(text)
+        return decoded
 
     def vocab(self):
         return self.tokenizer.get_vocab()
@@ -80,22 +109,24 @@ class WordPieceTokenizer:
     def save(self, filename):
         self.tokenizer.save(f"{filename}.json")
 
+    def __len__(self):
+        return self.size
+
 def load_tokenizer(filename):
     return Tokenizer.from_file(f"{filename}.json")
 
 
 if __name__ == "__main__":
-    corpus = ["hello my name is doug", "doug is a dog who is a good boy", 
-            "this is just a small example of testing the encoder", 
-            "just another piece of information where we can byte-pair encode",
-        "another day, another dollar you know what they say"]
-    vocab_size = 100
-    norms = normalizers.Sequence([normalizers.Strip(), normalizers.Lowercase(), 
-                                normalizers.StripAccents(), normalizers.NFD()])
-    pretoks = pre_tokenizers.Whitespace()
-    special_tokens = dict(unk="[UNK]", cls="[CLS]", sep="[SEP]", 
-                        pad="[PAD]", mask="[MASK]")
-    tokenizer = WordPieceTokenizer(vocab_size, norms, pretoks, "##", special_tokens=special_tokens, maxlen=100)
+    corpus = ["This is an escence to the beginning of what is Physics II. Phyics\
+         II is a great course for learning how the things we work with every day\
+             tend to work and operate. There's many special cases in which it is used"]
+    tokenizer = WordPieceTokenizer()
+    tokenizer.train(100, corpus)
+    tokenizer.pruncate()
+    print(tokenizer.encode("pad at all now its too long"))
+    
+
+
 
     
     
--- Transformer/utils.py
+++ Transformer/utils.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
-from torch.utils.data import TensorDataset, DataLoader
+import pandas as pd
+from torch.utils.data import TensorDataset, DataLoader, IterableDataset
+from tokenizer import WordPieceTokenizer
 
 def generate_masks(source, targets, pad_idx):
     # inshape source: (batch_size, inputs_len) targets: (batch_size, outputs_len) pad_idx: (,)
@@ -31,90 +33,39 @@ def generate_nopeak_pad_mask(tgt, pad_idx):
     tgt_mask = tgt_mask & tgt_nopeak_mask
     return tgt_mask
 
-def pad_tokens(tokens, pad_idx, end=True):
-    # inshape (n, arbitrary)
-    padded = []
-    # find max len in tokens
-    maxlen = len(max(tokens, key=len))
-    # pad all seqs in tokens
-    for seq in tokens:
-        seq = np.array(seq)
-        seq = pad_sequence(seq, maxlen, pad_idx, end=end)
-        padded.append(seq)
-    # return as array of ints
-    return np.array(padded, dtype=np.float64)
-
-def pad_sequence(sequence, maxlen, pad_idx, end=True):
-    seq_len = len(sequence)
-    # pad the sequence if it's smaller than maxlen
-    if seq_len < maxlen:
-        # create pad
-        pad = np.zeros((maxlen - seq_len,)) + pad_idx
-        # add pad to start or end
-        sequence = np.append(sequence, pad) if end else np.append(pad, sequence)
-    return sequence
-
-def pad_outputs(tokens_1, tokens_2, pad_idx, end=True):
-    # find max len between the pair of tokens
-    maxlen = max(len(max(tokens_1, key=len)), len(max(tokens_2, key=len)))
-    padded_1, padded_2 = [], []
-    # pad all seqs in both tokens with same maxlen
-    for seq_1, seq_2 in zip(tokens_1, tokens_2):
-        seq_1, seq_2 = np.array(seq_1), np.array(seq_2)
-        seq_1 = pad_sequence(seq_1, maxlen, pad_idx, end)
-        seq_2 = pad_sequence(seq_2, maxlen, pad_idx, end)
-        padded_1.append(seq_1), padded_2.append(seq_2)
-        # return as array of ints for both 
-    return np.array(padded_1, dtype=np.int64), np.array(padded_2, dtype=np.int64)
-
-def truncate_tokens(tokens, maxlen, delete=False):
-    truncated = []
-    # delete seqs that exceed maxlen
-    if delete:
-        for seq in tokens:
-            # only add seq if in limits of maxlen
-            if len(seq) <= maxlen:
-                truncated.append(seq)
-    # truncate seqs that exceed maxlen
-    else:
-        for seq in tokens:
-            seq = np.array(seq)
-            # truncate seq larger than maxlen
-            if len(seq) > maxlen:
-                seq = seq[:maxlen]
-            truncated.append(seq)
-    # return as array of objects
-    return np.array(truncated, dtype=object)
-
-def truncate_outputs(tokens_1, tokens_2, maxlen, delete=False):
-    truncated_1, truncated_2 = [], []
-    # delete seqs that exceed maxlen
-    if delete:
-        for seq_1, seq_2 in zip(tokens_1, tokens_2):
-            # only add seq pair if in limits of maxlen
-            if len(seq_1) <= maxlen >= len(seq_2):
-                truncated_1.append(seq_1), truncated_2.append(seq_2)
-        return np.array(truncated_1, dtype=object), np.array(truncated_2, dtype=object)
-    # truncate seqs that exceed maxlen and return as array of objects for both
-    return truncate_tokens(tokens_1, maxlen, delete=False), truncate_tokens(tokens_2, maxlen, delete=False)
-
-def ascending_data(vocab_size, n, maxlen, sos, eos, pad_idx):
-    inputs, outputs = [], []
-    base = np.array([sos, eos])
-    token_set = set(range(n))
-    for i in range(n):
-        seq_len = np.random.randint(1, maxlen + 1)
-        token = np.random.randint(1, vocab_size) - maxlen
-        token *= -1 if token < 0 else 1
-        seq = np.arange(token, token + seq_len)
-        seq = np.insert(base, 1, seq)
-        src, tgt = seq, seq[::-1]
-        inputs.append(src), outputs.append(tgt)
-
-    inputs = pad_tokens(inputs, pad_idx=pad_idx, end=True)
-    outputs = pad_tokens(outputs, pad_idx=pad_idx, end=True)
-    return inputs, outputs
         
+class Dataset(IterableDataset):
+    
+    def __init__(self, inputs, labels, vocab_size, tokenizer_kwargs=None):
+        self.size = len(inputs)
+        self.data = inputs, labels
+        self. tokenizer = self.__init_tokenizer(inputs, labels, vocab_size)
+        self.inputs = self.__tokenize(inputs)
+        self.labels = self.__tokenize(labels)
+
+    def __init_tokenizer(self, inputs, labels, vocab_size):
+        tokenizer = WordPieceTokenizer()
+        corpus = list(inputs) + list(labels)
+        tokenizer.train(vocab_size, corpus)
+        return tokenizer
+
+    def __tokenize(self, data):
+        return self.tokenizer.encode(data, model=True), self.tokenizer.encode(data, model=False)
+
+    def get_inputs(self, model=False):
+        if model:
+            return self.inputs[0]
+        return self.inputs[1]
+
+    def get_labels(self, model=False):
+        if model:
+            return self.labels[0]
+        return self.labels[1]
+
+    def get_tokenizer(self):
+        return self.tokenizer
+
+    
 def create_dataloader(inputs, labels, batch_size=32, drop_last=True, shuffle=False, **dataloader_kwargs):
     # create tensors
     inputs, labels = torch.from_numpy(inputs), torch.from_numpy(labels)
@@ -126,4 +77,5 @@ def create_dataloader(inputs, labels, batch_size=32, drop_last=True, shuffle=Fal
 
 if __name__ == "__main__":
     pass
+
     
