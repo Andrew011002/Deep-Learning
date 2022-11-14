107796a9ce04a2539ca2deb45539729db1f65560
andmholm <andmholm@iu.edu>
2022-11-13 22:17:06 -0500

    dataset class for training transformer

--- Transformer/utils.py
+++ Transformer/utils.py
@@ -33,40 +33,89 @@ def generate_nopeak_pad_mask(tgt, pad_idx):
     tgt_mask = tgt_mask & tgt_nopeak_mask
     return tgt_mask
 
-        
 class Dataset(IterableDataset):
     
-    def __init__(self, inputs, labels, vocab_size, tokenizer_kwargs=None):
+    def __init__(self, inputs, labels):
+        if len(inputs) != len(labels):
+            raise ValueError(f"the size of inputs ({len(inputs)}) \
+must match the size of labels ({len(labels)})")
+        if type(inputs[0]) != type(labels[0]):
+            raise ValueError(f"the data type of inputs ({type(inputs[0])}) \
+must match the data type of labels ({type(labels[0])})")
+        self.inputs = inputs
+        self.labels = labels
         self.size = len(inputs)
-        self.data = inputs, labels
-        self. tokenizer = self.__init_tokenizer(inputs, labels, vocab_size)
-        self.inputs = self.__tokenize(inputs)
-        self.labels = self.__tokenize(labels)
+        self.dtype = type(inputs[0])
 
-    def __init_tokenizer(self, inputs, labels, vocab_size):
-        tokenizer = WordPieceTokenizer()
-        corpus = list(inputs) + list(labels)
-        tokenizer.train(vocab_size, corpus)
-        return tokenizer
+    # returns data as list
+    def list(self):
+        if isinstance(self.inputs, torch.Tensor) or isinstance(self.inputs, np.ndarray):
+            return self.inputs.tolist(), self.labels.tolist()
+        return self.inputs, self.labels
 
-    def __tokenize(self, data):
-        return self.tokenizer.encode(data, model=True), self.tokenizer.encode(data, model=False)
+    # returns data as tensors
+    def tensors(self):
+        if self.dtype == str or self.dtype == np.str_:
+            raise TypeError(f"torch does not support type {self.dtype}")
+        if isinstance(self.inputs, np.ndarray):
+            return torch.from_numpy(self.inputs), torch.from_numpy(self.labels)
+        return torch.tensor(self.inputs), torch.tensor(self.labels)
 
-    def get_inputs(self, model=False):
-        if model:
-            return self.inputs[0]
-        return self.inputs[1]
+    # returns data as numpy array
+    def numpy(self):
+        if isinstance(self.inputs, torch.Tensor):
+            return self.inputs.numpy(), self.labels.numpy()
+        return np.array(self.inputs), np.array(self.labels)
 
-    def get_labels(self, model=False):
-        if model:
-            return self.labels[0]
-        return self.labels[1]
+    # returns the entire dataset as a corpus
+    def corpus(self):
+        # combine correct type object
+        if isinstance(self.inputs, np.ndarray):
+            return np.append(self.inputs, self.labels)
+        elif isinstance(self.inputs, torch.Tensor):
+            return torch.cat((self.inputs, self.labels), dim=0)
+        return self.inputs + self.labels
 
-    def get_tokenizer(self):
-        return self.tokenizer
+    def sample(self):
+        return np.random.choice(self.inputs), np.random.choice(self.labels)
 
-    
+    # returns a sampled batch of specified batch size
+    def batch(self, batch_size):
+        if batch_size > self.size:
+            raise ValueError(f"cannot sample batch larger than size ({self.size})")
+        input_sample, label_sample = np.random.choice(self.inputs, (batch_size, ), replace=False),\
+            np.random.choice(self.inputs, (batch_size, ), replace=False)
+        
+        # return correct list object
+        if isinstance(self.inputs, torch.Tensor):
+            return torch.from_numpy(input_sample), torch.from_numpy(label_sample)
+        elif isinstance(self.inputs, list):
+            return list(input_sample), list(label_sample)
+        return input_sample, label_sample
+
+
+    # returns a dataframe of data
+    def dataframe(self, headers=None):
+        if headers is None:
+            headers = ("inputs", "labels")
+        d = {f"{headers[0]}": self.inputs, f"{headers[1]}": self.labels}
+        return pd.DataFrame(d)
+
+    def __getitem__(self, index):
+        return self.inputs[index], self.labels[index]
+
+    def __iter__(self):
+        return iter([(input, label) for input, label in zip(self.inputs, self.labels)])
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        return str(self.dataframe())
+
+
 def create_dataloader(inputs, labels, batch_size=32, drop_last=True, shuffle=False, **dataloader_kwargs):
+    inputs, labels = np.array(inputs), np.array(labels)
     # create tensors
     inputs, labels = torch.from_numpy(inputs), torch.from_numpy(labels)
     tensorset = TensorDataset(inputs, labels)
