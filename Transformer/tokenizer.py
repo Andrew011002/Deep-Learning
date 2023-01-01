e453f651c94b4238debac6d08a0f945db581aa06
andmholm <andmholm@iu.edu>
2023-01-01 10:20:36 -0500

    fixed comments

--- Transformer/tokenizer.py
+++ Transformer/tokenizer.py
@@ -15,7 +15,6 @@ def basic_tokenizer(vocab, prefix, special_tokens):
 class BaseTokenizer:
 
     def __init__(self, vocab=None, prefix=None, special_tokens=None):
-
         # defaults
         if prefix is None:
             prefix = "##"
@@ -23,7 +22,7 @@ class BaseTokenizer:
             special_tokens = {"start": "[CLS]", "end": "[SEP]", "unknown": "[UNK]", 
                 "pad": "[PAD]", "mask": "[MASK]"}
         
-        # set tokenizer and store keyword arguments
+        # set tokenizer & store keyword arguments
         self.tokenizer = basic_tokenizer(vocab, prefix, special_tokens)
         self.kwargs = dict(vocab=vocab, prefix=prefix, 
                     special_tokens=special_tokens)
@@ -38,7 +37,7 @@ class BaseTokenizer:
                     special_tokens=list(self.kwargs["special_tokens"].values()), 
                     continuing_subword_prefix=self.kwargs["prefix"])
 
-        # train and set sequence processor
+        # train & set sequence processor
         self.tokenizer.train_from_iterator(corpus, trainer)
         start, end = self.kwargs["special_tokens"]["start"], self.kwargs["special_tokens"]["end"]
         start_id, end_id = self.tokenizer.token_to_id(start), self.tokenizer.token_to_id(end)
@@ -47,7 +46,6 @@ class BaseTokenizer:
                 pair=f"{start}:0 $A:0 {end}:0 $B:1 {end}:1",
                 special_tokens=[(start, start_id), (end, end_id)])
 
-    # encodes input to tokens or ids
     def encode(self, data, model=False, **kwargs):
         # handle single sequence or pair
         if isinstance(data, str):
@@ -66,34 +64,28 @@ class BaseTokenizer:
                 encoded.append(encoding.tokens)
         return encoded
         
-    # decodies ids to tokens
     def decode(self, data, special_tokens=False, **kwargs):
         # decode the sequence(s)
         tokens = self.tokenizer.decode_batch(data, skip_special_tokens=not special_tokens)
         return tokens
 
-    # enables padding
     def padon(self, maxlen=None, end=True, pad_id=0):
         end = "right" if end else "left"
         pad = self.tokenizer.id_to_token(pad_id)
         self.tokenizer.enable_padding(direction=end, pad_id=self[pad],
                         pad_token=pad, length=maxlen)
 
-    # enables truncation
     def truncon(self, maxlen, end=True):
         end = "right" if end else "left"
         self.tokenizer.enable_truncation(max_length=maxlen, direction=end)
 
-    # disables padding and truncation for sequence inputs 
     def inference(self):
         self.tokenizer.no_padding()
         self.tokenizer.no_truncation()
 
-    # returns entire vocab
     def vocab(self):
         return self.tokenizer.get_vocab()
 
-    # adds tokens to vocabulary
     def add(self, tokens):
         self.tokenizer.add_tokens(tokens)
 
@@ -103,27 +95,25 @@ class BaseTokenizer:
     def get_special_tokens(self):
         return self.kwargs["special_tokens"]
 
-    # total vocabulary
     def __len__(self):
         return self.tokenizer.get_vocab_size()
 
-    # gets id of token in vocab
-    def __getitem__(self, token):
-        if isinstance(token, int):
-            return self.tokenizer.id_to_token(token)
-        elif isinstance(token, str):
+    def __getitem__(self, value):
+        # try to get token or token_id of value
+        if isinstance(value, int):
+            return self.tokenizer.id_to_token(value)
+        elif isinstance(value, str):
             vocab = self.vocab()
-            return vocab.get(token, KeyError(f"{token} not in vocab"))
+            return vocab.get(value, KeyError(f"{value} not in vocab"))
 
-    # trys to encode then decode if possible
     def __call__(self, data, model=False, special_tokens=False, **kwargs):
+        # trys to encode then decode if possible
         try: return self.encode(data, model=model)
         except: 
             try: return self.decode(data, special_tokens=special_tokens)
             except:
                 raise ValueError(f"Can't interpret input {data}")
 
-# my custom tokenizer
 class Nerdimizer(BaseTokenizer):
 
     def __init__(self, vocab=None):
@@ -137,6 +127,7 @@ class Translator:
         self.tokenizer_decoder = tokenizer_decoder
 
     def encode(self, data, model=True, module=None):
+        # encode based on module 
         if module == "encoder":
             return self.tokenizer_encoder.encode(data, model=model)
         if module == "decoder":
@@ -145,6 +136,7 @@ class Translator:
             raise ValueError(f"Unknown argument: {module}")
 
     def decode(self, data, special_tokens=False, module=None):
+        # decode based on module
         if module == "encoder":
             return self.tokenizer_encoder.decode(data, special_tokens=special_tokens)
         if module == "decoder":
@@ -170,27 +162,23 @@ class Translator:
     def __getitem__(self, token):
         return self.tokenizer_encoder[token]
 
-# saves tokenizer as json
 def save_tokenizer(tokenizer, path=None):
     # default
     path = "tokenizer" if path is None else path
-
     # create path if non-existant
     create_path(path)
-
     # save tokenizer to path
     tokenizer.tokenizer.save(f"{path}.json")
     print(f"Tokenizer saved")
 
-# loads saved tokenizer
-def load_tokenizer(path=None):
+def load_tokenizer(path=None, verbose=True):
     # default
     path = "tokenizer" if path is None else path
-
     # set tokenizer for base tokenizer from path
     tokenizer = BaseTokenizer()
     tokenizer.set_tokenizer(Tokenizer.from_file(f"{path}.json"))
-    print(f"Tokenizer loaded")
+    if verbose:
+        print(f"Tokenizer loaded")
     return tokenizer
 
 if __name__ == "__main__":
--- Transformer/utils.py
+++ Transformer/utils.py
@@ -15,49 +15,45 @@ def model_size(model):
     buffer_size_mb = sum(buffer.nelement() * buffer.element_size() for buffer in buffers)
     return (param_size_mb + buffer_size_mb) / np.power(1024, 2)
 
-def generate_masks(source, targets, pad_id):
-    # inshape source: (batch_size, inputs_len) targets: (batch_size, outputs_len) pad_id: (,)
-    tgt_len = targets.size(1)
+def generate_masks(src, tgt, pad_id):
+    # inshape: src - (batch_size, src_len) tgt - (batch_size, tgt_len)
 
-    # create padded mask for src & tgt 
-    src_mask = (source != pad_id).unsqueeze(-2)
-    tgt_mask = (targets != pad_id).unsqueeze(-2)
+    # create padded mask for src (True = no pad False = pad) shape: src_mask - (batch_size, 1, src_len) 
+    src_mask = (src != pad_id).unsqueeze(-2)
 
-    # create subsequent mask for tgt (no peak) shape tgt_nopeak_mask: (1, tgt_len, tgt_len)
-    tgt_nopeak_mask = torch.triu(torch.ones((1, tgt_len, tgt_len)) == 1)
-    tgt_nopeak_mask = tgt_nopeak_mask.transpose(1, 2)
-    
-    # combine tgt_pad_mask & tgt_nopeak_mask to hide pad and prevent subsequent attention
-    tgt_mask = tgt_mask & tgt_nopeak_mask
-    # shape src_mask: (batch_size, 1, seq_len) tgt_mask: (batch_size, tgt_len, tgt_len)
+    # generate padded nopeak mask for tgt
+    tgt_mask = generate_nopeak_pad_mask(tgt, pad_id)
     return src_mask, tgt_mask
 
 def generate_nopeak_pad_mask(tgt, pad_id):
-    # inshape tgt: (batch_size, tgt_len)
-    tgt_len = tgt.size(1)
-    # padded mask (True where no pad False otherwise)
+    # inshape: tgt - (batch_size, tgt_len)
+
+    # create padded mask for src (True = no pad False = pad) shape: tgt_mask - (batch_size, 1, tgt_len)
     tgt_mask = (tgt != pad_id).unsqueeze(-2)
-    # create subsequent mask
+    # create subsequent mask shape: tgt_nopeak_mask - (1, tgt_len, tgt_len)
+    tgt_len = tgt.size(1)
     tgt_nopeak_mask = torch.triu(torch.ones((1, tgt_len, tgt_len)) == 1)
     tgt_nopeak_mask = tgt_nopeak_mask.transpose(1, 2)
-    # combine padded & subsequent mask
+    # combine padded & subsequent mask shape: tgt_mask - (batch_size, tgt_len, tgt_len)
     tgt_mask = tgt_mask & tgt_nopeak_mask
     return tgt_mask
 
-# splits sequence pairs up to a desired size from a dataset
 def get_split(datadict, input_key, label_key, size=100000):
-    inputs, labels = [], []
+    # assort through all pairs until size size is met or end
+    inputs, labels = set(), set()
     count = 0
-    # add sequence pairs while the desired size isn't reached
+
     for pair in datadict["translation"]:
-        if pair[input_key] != pair[label_key]:
-            inputs.append(pair[input_key])
-            labels.append(pair[label_key])
+        input, label = pair[input_key], pair[label_key]
+        # add non-duplicates & unique pairs
+        if input != label and input not in inputs and label not in labels:
+            inputs.add(pair[input_key])
+            labels.add(pair[label_key])
             count += 1
         # size reached
         if count == size:
             break
-    return inputs, labels
+    return list(inputs), list(labels)
 
 class Dataset(IterableDataset):
 
@@ -73,13 +69,11 @@ must match the data type of labels ({type(labels[0])})")
         self.size = len(inputs)
         self.dtype = type(inputs[0])
 
-    # returns data as list
     def list(self):
         if isinstance(self.inputs, torch.Tensor) or isinstance(self.inputs, np.ndarray):
             return self.inputs.tolist(), self.labels.tolist()
         return self.inputs, self.labels
 
-    # returns data as tensors
     def tensors(self):
         if self.dtype == str or self.dtype == np.str_:
             raise TypeError(f"PyTorch does not support type {self.dtype}")
@@ -87,39 +81,36 @@ must match the data type of labels ({type(labels[0])})")
             return torch.from_numpy(self.inputs), torch.from_numpy(self.labels)
         return torch.tensor(self.inputs), torch.tensor(self.labels)
 
-    # returns data as numpy array
     def numpy(self):
         if isinstance(self.inputs, torch.Tensor):
             return self.inputs.numpy(), self.labels.numpy()
         return np.array(self.inputs), np.array(self.labels)
 
-    # returns the entire dataset as a corpus
-    def corpus(self, split=2):
-        if split < 0 or split > 2:
-            raise ValueError(f"Invalid input for split: {split} (0 = inputs, 1 = labels, 2 = both)")
+    def corpus(self, data=None):
+        if data != "inputs" and data != "labels" and data is not None:
+            raise ValueError(f"Unknown argument for data: {data}\
+Valid arguments: (inputs, labels, both)")
 
-        # return proper split
+        # return the proper corpus
+        data = "both" if data is None else data
         inputs, labels = self.list()
-        # inputs only
-        if split == 0:
+        if data == "inputs":
             return inputs
-        # labels only
-        if split == 1:
+        if data == "labels":
             return labels
-        # both
-        return inputs + labels
+        if data == "both":
+            return inputs + labels
 
-    # returns a sampled batch of specified batch size
     def sample(self, n=1):
         if n > self.size:
             raise ValueError(f"Cannot sample batch larger than size ({self.size})")
+
         # get random slices
         indices = np.random.choice(len(self), (n, ), replace=False).astype(int)
         inputs, labels = self.inputs, self.labels
         # return as pairs
         return [(inputs[i], labels[i]) for i in indices]
 
-    # returns a dataframe of data
     def dataframe(self, headers=None):
         if headers is None:
             headers = ("inputs", "labels")
@@ -138,25 +129,24 @@ must match the data type of labels ({type(labels[0])})")
     def __str__(self):
         return str(self.dataframe())
 
-    # gives back a tokenized dataset
     def tokenized(self, tokenizer, model=True):
+        # encode inputs & labels
         inputs, labels = self.list()
         input_tokens, label_tokens = tokenizer.encode(inputs, model=model, module="encoder"), \
             tokenizer.encode(labels, model=model, module="decoder")
+        # return as Dataset
         return Dataset(input_tokens, label_tokens)
 
-    # finds the average length of tokenized sequences
     def avglen(self, tokenizer, factor=1):
+        # find max between avg input length & avg label length
         inputs, labels = self.list()
-        # give back higher average of average lengths of sequences
         m = sum(len(input) for input in \
             tokenizer.encode(inputs, model=True, module="encoder")) / self.size
-        n = sum(len(label) for label in\
+        n = sum(len(label) for label in \
             tokenizer.encode(labels, model=True, module="decoder")) / self.size
         # apply factor to incorporate outlier sequences
         return int(np.rint(max(m, n)) * factor)
     
-    # gives back maxlen between tokenized sequences
     def maxlen(self, tokenizer, factor=1):
         inputs, labels = self.list()
         # find max longest sequence in inputs & labels
@@ -199,12 +189,11 @@ class Checkpoint:
         return False
 
     def save(self):
-        # give name
+        # default
         if self.path is None:
             self.path = "checkpoint"
-        # create path if not existent 
+        # create path (if non-existent)
         create_path(self.path)
-
         # save same path
         if self.overwrite:
             path = f"{self.path}.pt"
@@ -212,7 +201,6 @@ class Checkpoint:
         else:
             path = f"{self.path}-{self.epoch}.pt"
 
-        # save params of
         torch.save({
             # required
             "model_params": self.model.state_dict(),
@@ -234,6 +222,7 @@ class Checkpoint:
         }, path)
             
     def load_checkpoint(self, path=None, verbose=True, device=None): 
+        # default
         if path is None:
             path = "checkpoint"
         # load checkpoint
@@ -256,7 +245,6 @@ class Checkpoint:
             self.bleu = checkpoint["bleu"]
         if checkpoint["duration"] is not None:
             self.clock = Clock(checkpoint["duration"])
-
         if verbose:
             print("Checkpoint Loaded")
     
@@ -297,7 +285,7 @@ class Clock:
         return self.asstr(h, m, s)
         
     def reset(self):
-        self.__init__()
+        self.__init__(duration=0)
 
     def to_hour_min_sec(self, elapsed):
         hours, rem = elapsed // 3600, elapsed % 3600
@@ -310,23 +298,18 @@ class Clock:
         sstr = f"0{seconds:.0f}" if np.rint(seconds) < 10 else f"{seconds:.0f}"
         return f"{hstr}:{mstr}:{sstr}"
 
-# saves the model to a path
 def save_model(model, path=None):
     # default
     path = "model" if path is None else path
-
-    # create path if non-existant
+    # create path (if non-existent)
     create_path(path)
-    
     # save model to the path
     torch.save(model.state_dict(), f"{path}.pth")
     print(f"Model params saved")
 
-# loads a model params from a path
 def load_model(model, path=None, device=None):
     # default
     path = "model" if path is None else path
-
     # load parameters into model
     params = torch.load(f"{path}.pth", map_location=device)
     if torch.cuda.is_available():
@@ -334,7 +317,6 @@ def load_model(model, path=None, device=None):
     print(f"Model params loaded")
     return model
 
-# creates a path if it doesn't exist
 def create_path(path):
     path = path.split("/")
     path = "/".join(path[:-1]) + "/"
