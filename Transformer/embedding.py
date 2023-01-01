0f034d7f94d36d45fa31f5eb3d09c623230656eb
andmholm <andmholm@iu.edu>
2023-01-01 14:58:31 -0500

    comments

--- Transformer/embedding.py
+++ Transformer/embedding.py
@@ -2,16 +2,16 @@ import torch.nn as nn
 
 class Embeddings(nn.Module):
 
-    def __init__(self, n_tokens, dm, pad_id, **kwargs) -> None:
+    def __init__(self, vocab_size, dm, pad_id, **kwargs) -> None:
         super().__init__()
-        self.embedding = nn.Embedding(n_tokens, dm, pad_id, **kwargs)
+        self.embedding = nn.Embedding(vocab_size, dm, pad_id, **kwargs)
 
     def forward(self, x):
         # inshape: x - (batch_size, seq_len)
         return self.embedding(x)
     
     def unembedding(self):
-        # weight matrix for unembedding
+        # unembed tensor shape: weight - (vocab_size, dm)
         return self.embedding.weight
 
 
