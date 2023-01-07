08c6d2a08763a35ba7dcae599790f6b0d959f639
andmholm <andmholm@iu.edu>
2023-01-06 19:52:58 -0500

    config params

--- Transformer/config.py
+++ Transformer/config.py
@@ -5,8 +5,8 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 # data configurations
 train_size = 100000
 test_size = 1000
-vocab_size_english = 25000
-vocab_size_german = 25000
+vocab_size_english = 32000
+vocab_size_german = 32000
 maxlen = 256
 batch_size = 128
 
@@ -18,15 +18,25 @@ dff = 2048
 bias = False
 dropout = 0.1
 eps = 1e-5
+# optimizer configurations
 adam_eps = 10e-9
 lr = 1e-5
 betas = (0.9, 0.98)
+# scheduler
 factor = 0.9
 patience = 10
+# decoder search configurations
+beam_width = 3
+max_breadth = 100
+search_mode = "best"
+alpha = 0.6
 
 # training & metric configurations
+sample_size = 10
+bleu_goal = 23
 ngrams = 4
 save_every = 5
+overwrite = False
 warmups = 100
 epochs = 1000
 verbose = True
